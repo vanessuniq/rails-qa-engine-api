8f7b46213f988c1987602d78704b37a6c9e18923
vanessuniq <vanessuniq@gmail.com>
2020-11-05 12:01:40 -0500

    configured jwt current user and login requirements

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,9 @@ class UsersController < ApplicationController
     def create
         user = User.new(user_params)
         if user.save
-            render json: UserSerializer.new(user).serializable_hash
+            payload = {user_id: user.id}
+            token = encode_token(payload)
+            render json: UserSerializer.new(user).serializable_hash, jwt: token
         else
             render json: {errors: user.errors.full_messages}, status: :not_acceptable
         end
