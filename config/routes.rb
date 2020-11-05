076f8ee8f5f1b9c43e6fb91e3ceab9f2c6ae42ba
vanessuniq <vanessuniq@gmail.com>
2020-11-05 10:36:25 -0500

    updated authentication routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root to: "static#home"
   resources :questions, except: [:new, :show, :edit, :update]
   resources :answers, except: [:new, :edit, :show]
-  resources :users, only: [:create]
-  post '/login', to: "auth#login"
-  get '/profile', to: "users#profile"
+  resources :users, only: [:create] #handle signup
+  post '/login', to: "auth#login" #handles login for existing users
+  get '/auto_login', to: 'auth#auto_login' #handles auto login upon successful signup/login
+  get '/user_is_authed', to: 'auth#user_is_authed' #only accessible to authorized users
 end
