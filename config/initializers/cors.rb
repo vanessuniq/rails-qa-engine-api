4275888ce3793c6935957736827b9a1ee224bb67
vanessuniq <vanessuniq@gmail.com>
2020-12-21 23:49:35 -0500

    changed cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -22,12 +22,5 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
       credentials: true
   end
-  allow do
-    origins "https://web.postman.co"
-
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
-  end
+  
 end
